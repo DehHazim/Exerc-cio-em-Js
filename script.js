51850c83aa7ae9cf865970739ab410ba74d3e2a0
Deborah Hazim <deborahhhazim@gmail.com>
2025-04-07 21:23:38 -0300

    algumas modificações

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function carregar() {
-    var msg = window.document.getElementById("msg")
-    var img = window.document.getElementById("imagem")
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     var hora = 22
-    msg.innerHTML = "Agora são ${hora} horas."
-    if (hora <= 0 && < 12){
+    msg.innerHTML = 'Agora são ${hora} horas.'
+    if (hora >= 0 && < 12){
       // Bom dia
-      img.src = "manha.png"       
+      img.src = 'manha1.jpg'       
       document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
       // Boa tarde
-      img.src = "tarde1.png"
+      img.src = 'tarde1.jpg'
         document.body.style.backgroung = '#b9846f'
     } else {
-      img.src = "noite1.png"
-        document.body.style.background = #515154
+      img.src = 'noite1.jpg'
+        document.body.style.background = '#515154'
     }
-    
+   
   }
